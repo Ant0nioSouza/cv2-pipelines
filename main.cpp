fe9417de8d3123800e778bcedf51bba446bdbdfe
Antonio Souza <antoniosouza.dev@gmail.com>
2022-02-27 20:55:10 -0300

    feat first step of recognize method

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <string>
 
 #define PIC_PATH "/home/isomeister/pic1.png"
 
@@ -23,6 +24,19 @@ void takePicture(Mat video, Size resolution) {
     imwrite(PIC_PATH, dst);
 }
 
+void recognize(string path, bool redSide) {
+    Mat res, gray, tresh;
+    
+    resize(imread(path), res, Size(200, 200));
+    cvtColor(res, gray, COLOR_BGR2GRAY);
+    threshold(gray, gray, 80, 255, 0);
+
+    imshow("resized", res);
+    imshow("marked", gray);
+    //imshow("treshold", tresh);
+    waitKey(0);
+}
+
 int main(int, char **) {
 
     Mat source;
@@ -30,6 +44,8 @@ int main(int, char **) {
     
     cout << "Capturing image...";
     takePicture(source, Size(150, 200));
+    cout << "Recognizing image...";
+    recognize("/home/isomeister/Pictures/Screenshot from 2022-02-27 19-24-12.png", false);
 
     return 0;
 }
