63b7ecf22ac002e55939c7046e8061484a8f3817
Antonio Souza <antoniosouza.dev@gmail.com>
2022-02-27 18:17:27 -0300

    build initial cmake project

---
+++ build/CMakeFiles/cv2-pipelines.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv2-pipelines.dir/main.cpp.o"
+  "cv2-pipelines"
+  "cv2-pipelines.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv2-pipelines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/isomeister/Documents/xmachine/code/cv2-pipelines
+# Build directory: /home/isomeister/Documents/xmachine/code/cv2-pipelines/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
