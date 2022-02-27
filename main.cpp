6f0b81728619f35482d7bcac9f5f26ea541276fa
Antonio Souza <antoniosouza.dev@gmail.com>
2022-02-27 19:01:18 -0300

    save a determinated frame in the storage method

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+#define PIC_PATH "/home/isomeister/pic1.png"
+
+using namespace std;
+using namespace cv;
+
+
+VideoCapture cap(0);
+
+/**
+ * @brief save the gived frame in a file.
+ * 
+ * @param video The frame in format of cv::Mat.
+ * @param resolution The resolution that the picture must be saved.
+ */
+void takePicture(Mat video, Size resolution) {
+    Mat dst;
+    resize(video, dst, resolution);
+    imwrite(PIC_PATH, dst);
+}
+
+int main(int, char **) {
+
+    Mat source;
+    cap.read(source);
+    
+    cout << "Capturing image...";
+    takePicture(source, Size(150, 200));
+
+    return 0;
 }
